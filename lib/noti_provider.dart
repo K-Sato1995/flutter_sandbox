6d09ccbfb755c3e96034d763c6e63da3ca7dfcc8
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-11-09 15:52:40 +0900

    Add models

---
+++ lib/noti_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// https://github.com/altive/flutter_app_template/tree/main/packages/flutter_app/lib/src/pages/riverpod_example_page/state_notifier_provider_page
+class TodoNotifier extends StateNotifier<List<Todo>> {
+  TodoNotifier() : super([]);
+
+  void add(Todo todo) {
+    state = [...state, todo];
+  }
+
+  void remove(Todo todo) {
+    state = state.where((element) => element != todo).toList();
+  }
+
+  void toggle(String todoId) {
+    state = [
+      for (final todo in state)
+        if (todo.id == todoId)
+          todo.copyWith(completed: !todo.completed)
+        else
+          todo,
+    ];
+  }
+}
